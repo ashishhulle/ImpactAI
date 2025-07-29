9e4534b5294d9140a15f74457807ac5d72d6cc79
ashishhulle <55648469+ashishhulle@users.noreply.github.com>
2025-07-29 15:04:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,42 @@
-const URL = 'https://teachablemachine.withgoogle.com/models/hFJ7H0itp/';
+const URL = 'https://teachablemachine.withgoogle.com/models/w8rORZlke/';
 
-const loadingStatus = document.querySelector('.loadingStatus .loadingContainer');
+const loadingStatus = document.querySelector('.loadingStatus .loadingConitainer');
 const uploadedImage = document.querySelector('#uploaded-image');
-const webcamElement = document.createElement('video');
-let model, labelContainer, maxPredictions, webcam;
+
+let model, labelContainer, maxPredictions;
 
 async function init() {
     const modelURL = URL + 'model.json';
     const metadataURL = URL + 'metadata.json';
+
+
     
-    // Load the model and metadata
+    // load the model and metadata
     model = await tmImage.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
 
-    // Append elements to the DOM
-    labelContainer = document.querySelector('#label-container .indicator');
-    for (let i = 0; i < maxPredictions; i++) {
+    // append elements to the DOM
+    labelContainer = document.querySelector('#label-container .indecator');
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
         labelContainer.appendChild(document.createElement('div')).innerHTML = '0%';
     }
-    
-    // Setup webcam
-    webcam = new tmImage.Webcam(200, 200, true); // width, height, flip
-    await webcam.setup(); // Request access to the webcam
-    await webcam.play();
-    document.querySelector('#webcam-container').appendChild(webcam.canvas);
-    window.requestAnimationFrame(loop);
 }
 
-async function loop() {
-    webcam.update(); // Update the webcam frame
-    await predict(webcam.canvas);
-    window.requestAnimationFrame(loop);
-}
-
-// Run the uploaded image through the model
+// run the uploaded image through the image model
 async function predict(imageElement) {
     const prediction = await model.predict(imageElement, false);
     for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction = prediction[i].className + ': ' + (prediction[i].probability * 100).toFixed(2) + '%';
+        const classPrediction =
+            prediction[i].className + ': ' + (prediction[i].probability*100).toFixed(2) + '%';
+        
         labelContainer.querySelectorAll('div')[i].innerHTML = classPrediction;
         labelContainer.querySelectorAll('div')[i].style.width = `${prediction[i].probability.toFixed(2) * 100}%`;
+        loadingStatus.style.display = 'none';
     }
-    loadingStatus.style.display = 'none';
 }
 
 document.getElementById('image-upload').addEventListener('change', function(event) {
-    document.getElementById('file-name').textContent = this.files[0].name;
+    document.getElementById('file-name').textContent = this.files[0].name;;
     loadingStatus.style.display = 'block';
     const file = event.target.files[0];
     const reader = new FileReader();
